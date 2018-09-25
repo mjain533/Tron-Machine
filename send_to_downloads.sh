2d27e479e8cf1f34eed1791486c3a5effe25ee85
justincarrington <justincarrington@justins-mbp-2>
2018-09-25 14:13:34 -0500

    Updating download system

--- send_to_downloads.sh
+++ send_to_downloads.sh
@@ -4,7 +4,7 @@ cd target
 
 quantFile=$(ls -p | grep -v / | sort -V | tail -n 1)
 echo $quantFile
-
+cd ..
 
 curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"target/$quantFile"
 
