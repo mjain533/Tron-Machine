dea6f81628a40cb7806347143eee3ff08a0e44f8
justincarrington <justincarrington@justins-mbp-2>
2018-09-25 14:08:38 -0500

    Try to make pipeline send build artifact to downloads

--- send_to_downloads.sh
+++ send_to_downloads.sh
@@ -4,7 +4,8 @@ cd target
 
 quantFile=$(ls -p | grep -v / | sort -V | tail -n 1)
 echo $quantFile
-cd ..
-mv target/$quantFile /
-ls
+
+
+curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"target/$quantFile"
+
 
