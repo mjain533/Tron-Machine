37cf673f0eeefa37769d301c667c6c33c111a647
justincarrington <justincarrington@justins-mbp-2>
2018-09-25 14:18:09 -0500

    Final changes to download mechanism

--- send_to_downloads.sh
+++ send_to_downloads.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd target
+cd JarBuilds
 
 quantFile=$(ls -p | grep -v / | sort -V | tail -n 1)
 echo $quantFile
 cd ..
 
-curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"target/$quantFile"
+curl -X POST "https://${BB_AUTH_STRING}@api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" --form files=@"JarBuilds/$quantFile"
 
 
