54eb9e10e111226abdcfe3c368f1fd8e1b167e44
justincarrington <justincarrington@10.141.94.198>
2018-09-26 02:23:18 -0500

    Update license

--- src/main/java/com/typicalprojects/TronMachine/neuronal_migration/Preferences2.java
+++ src/main/java/com/typicalprojects/TronMachine/neuronal_migration/Preferences2.java
@@ -1,3 +1,29 @@
+/*
+ * (C) Copyright 2018 Justin Carrington.
+ *
+ *  This file is part of TronMachine.
+
+ *  TronMachine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TronMachine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with TronMachine.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Justin Carrington
+ *     Russell Taylor
+ *     Kendra Taylor
+ *     Erik Dent
+ *     
+ */
+
 package com.typicalprojects.TronMachine.neuronal_migration;
 
 import java.awt.Component;
